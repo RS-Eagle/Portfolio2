c2e064f60805851644e75044362d1ac098b888f2
Raman Kumar Charak <withrkgaming78@gmail.com>
2024-10-23 13:00:37 +0530

    Added Side Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Skills from './components/Skills'
-import { useInView } from 'react-intersection-observer';
-import { useEffect, useState } from 'react';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+import Navbar_ul from "./components/Navbar_ul";
+import { motion } from "framer-motion";
 
 function App() {
-  const [curentNav,setCurrentNav] = useState("Home")
+  const [curentNav, setCurrentNav] = useState("Home");
   const { ref: ref1, inView: inView1 } = useInView({ threshold: 0.5 });
   const { ref: ref2, inView: inView2 } = useInView({ threshold: 0.5 });
   const [isScrolledDown, setIsScrolledDown] = useState(false);
 
-  useEffect(()=>{
-    if(inView1 && curentNav != "Home"){
-      setCurrentNav("Home")
+  useEffect(() => {
+    if (inView1 && curentNav != "Home") {
+      setCurrentNav("Home");
     }
-    if(inView2 && curentNav != "Skills"){
-      setCurrentNav("Skills")
+    if (inView2 && curentNav != "Skills") {
+      setCurrentNav("Skills");
     }
-  },[inView1,inView2])
-
-  
+  }, [inView1, inView2]);
 
   return (
     <>
-   
-      <Navbar props={{curentNav,setCurrentNav,isScrolledDown}}/>
+      <div className="fixed top-[20%] translate-y-[20%]   left-0 text-white  " >
+      <motion.div  
+    className="h-96 pb-5 flex items-end"
+    animate={{ x: !isScrolledDown ? -100 : 0, rotate: 90,y:-50 }}
+    transition={{ type: 'spring', stiffness: 300, damping: 20, delay: 0.1 }}>
+    <Navbar_ul props={{ curentNav, setCurrentNav, isScrolledDown }} />
+</motion.div>
+      </div>
+
+      <Navbar props={{ curentNav, setCurrentNav, isScrolledDown }} />
       <div>
         <div ref={ref1}>
-
-        <Hero props={{isScrolledDown,setIsScrolledDown}} />
+          <Hero props={{ isScrolledDown, setIsScrolledDown }} />
         </div>
         <div ref={ref2}>
-
-        <Skills  />
-        </div >
+          <Skills />
+        </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,18 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import Navbar_ul from './Navbar_ul'
 
 
 const Navbar = ({props}) => {
   
-  const li = ["Home","About","Projects","Skills"]
-
-
   return (
     <motion.nav animate={{ y: props.isScrolledDown ? -100 : 0 }}
     transition={{ type: 'spring', stiffness: 300, damping: 20,delay:0.1 }}
      className='fixed w-[100vw] bg-gray-900 text-white justify-center flex tab-break:justify-between p-6 px-10 items-center'>
         <h2 className='text-2xl cursor-pointer tab-break:block hidden '>Eagle's</h2>
-        <div className='bg-white/5 px-2 py-2 rounded-2xl shadow-lg backdrop-blur-sm  '>
-            <ul className='flex justify-center items-center *:px-4 *:py-2 *:cursor-pointer'>
-              {
-                li.map((e,i)=>{
-                  return (
-                    <AnchorLink className={`${props.curentNav==e? "bg-white text-black rounded-lg":"rounded-lg"} transition-all `} key={i} href={`#${e.toLowerCase()}`}>
-                    <li   onClick={()=>{props.setCurrentNav(e)}}>{e}</li></AnchorLink>
-                  )
-                })
-              }
-            </ul>
-        </div>
+            <Navbar_ul props={props}/>
+        
         <button className='p-3 px-5 tab-break:flex hidden bg-[#01acf8] border rounded-lg  items-center  border-[#01acf8]
           hover:bg-transparent transition-all duration-300'>Contact</button>
     </motion.nav>
